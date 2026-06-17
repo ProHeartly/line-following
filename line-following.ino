5083698ed1c65124bdd264529beb984cfb2639ce
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-06-17 16:29:19 +0545

    finilized the pins for arduino

--- line-following.ino
+++ line-following.ino
@@ -1,12 +1,33 @@
 // I'm not sure if I'm going to use esp32 devkit or arduino UNO for the actual project.. so I'm going to keep part for both of them :p
 
 
-void setup() {
-  // put your setup code here, to run once:
+// arduino---------------------------------------------------------------
+#define S1 A0
+#define S2 A1
+#define S3 A2
+#define S4 A3
+#define S5 A4
 
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 10
+
+float Kp = 35.0;
+float Ki = 0.0;
+float Kd = 20.0;
+
+int baseSpeed = 160;
+int maxSpeed = 230;
+int lastError = 0;
+
+
+void setup() {
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
