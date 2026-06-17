1fd6728e10cde9ad91180239bf6a7f1a68dfbfc0
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-06-17 12:20:58 +0545

    Initial commit

---
+++ line-following.ino
@@ -0,0 +1,12 @@
+// I'm not sure if I'm going to use esp32 devkit or arduino UNO for the actual project.. so I'm going to keep part for both of them :p
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
